346511a0c1a6d33ea25ae4941e488ec65adec69c
Bogusz <bognapierala@gmail.com>
2019-05-06 23:09:42 +0200

    Project setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom"
+
+import "styles/index.scss"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
