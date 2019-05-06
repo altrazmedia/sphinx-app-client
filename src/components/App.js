aa14e97f00383a2db3d349371b8905ebe38401ee
Bogusz <bognapierala@gmail.com>
2019-05-07 00:13:23 +0200

    Setting up redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux";
+
+import store from "store";
 
 import "styles/index.scss"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import rootReducer from "reducers";
+
+const middleware = [ thunk ];
+
+const composeEnhancers = process.env.NODE_ENV === "development" ? composeWithDevTools({}) || compose : compose;
+const store = createStore(rootReducer, {}, composeEnhancers(
+  applyMiddleware(...middleware)
+));
+
+export default store;
