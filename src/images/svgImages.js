bcab15694d93b2c55ce6242865a692e24623b95a
Bogusz <bognapierala@gmail.com>
2019-05-25 19:39:43 +0200

    More svg images

--- src/images/svgImages.js
+++ src/images/svgImages.js
@@ -2,3 +2,5 @@ export { ReactComponent as empty    } from "./empty.svg";
 export { ReactComponent as warning  } from "./warning.svg";
 export { ReactComponent as notfound } from "./404.svg";
 export { ReactComponent as hello    } from "./hello.svg";
+export { ReactComponent as choice   } from "./choice.svg";
+export { ReactComponent as search   } from "./search.svg";
