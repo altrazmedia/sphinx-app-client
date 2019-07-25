97c72bb07edfde6433c32429ee73de093ff69f2d
Bogusz <bognapierala@gmail.com>
2019-07-25 11:53:12 +0200

    Updates

---
+++ precache-manifest.a9e8f067990e50c2bc1ebbecb34f2458.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed62fa4501662637f3fa579bb9a41a0b",
+    "url": "/sphinx-app-client/index.html"
+  },
+  {
+    "revision": "4238dc02fd88fdbe657d",
+    "url": "/sphinx-app-client/static/css/main.cef17ba1.chunk.css"
+  },
+  {
+    "revision": "ab02e072c850167507a2",
+    "url": "/sphinx-app-client/static/js/2.ea79708b.chunk.js"
+  },
+  {
+    "revision": "4238dc02fd88fdbe657d",
+    "url": "/sphinx-app-client/static/js/main.c0c3eab0.chunk.js"
+  },
+  {
+    "revision": "6b021893c1b2bc7eb54e",
+    "url": "/sphinx-app-client/static/js/runtime~main.236a1395.js"
+  },
+  {
+    "revision": "7e78c4ae619320ed43bf09bb787403f5",
+    "url": "/sphinx-app-client/static/media/404.7e78c4ae.svg"
+  },
+  {
+    "revision": "1886877153058c07be49081b528a7359",
+    "url": "/sphinx-app-client/static/media/choice.18868771.svg"
+  },
+  {
+    "revision": "ffba73e61c2e0dda9cecb7ae30f7c0ce",
+    "url": "/sphinx-app-client/static/media/empty.ffba73e6.svg"
+  },
+  {
+    "revision": "0ea5dce06951fd06794c56a1f0191d48",
+    "url": "/sphinx-app-client/static/media/hello.0ea5dce0.svg"
+  },
+  {
+    "revision": "350c12da72e74f47e972337b0951db9e",
+    "url": "/sphinx-app-client/static/media/interface.350c12da.svg"
+  },
+  {
+    "revision": "9667b4c8c0cc9d06a4162250c9993779",
+    "url": "/sphinx-app-client/static/media/logo-inv.9667b4c8.png"
+  },
+  {
+    "revision": "a7e1386712138f2a26b9d9f15bc0f74b",
+    "url": "/sphinx-app-client/static/media/logo.a7e13867.png"
+  },
+  {
+    "revision": "deae67e12492330bff3149f1c8678972",
+    "url": "/sphinx-app-client/static/media/search.deae67e1.svg"
+  },
+  {
+    "revision": "a7fc19d7719f03b403aef0dd038d505b",
+    "url": "/sphinx-app-client/static/media/uk.a7fc19d7.png"
+  },
+  {
+    "revision": "98e9cdea82b6ab0024e7617775188295",
+    "url": "/sphinx-app-client/static/media/warning.98e9cdea.svg"
+  }
+]);
